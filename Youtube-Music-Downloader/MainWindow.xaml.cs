3948f36c79b32ff9ce40aa501da7ec6cd5957db2
LT <keinplan.luis@gmail.com>
2022-08-04 11:34:01 +0200

    Added UI

--- Youtube-Music-Downloader/MainWindow.xaml.cs
+++ Youtube-Music-Downloader/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,14 +14,60 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Youtube_Music_Downloader {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
+
         public MainWindow() {
             InitializeComponent();
+            initDownloadFolder();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e) {
+            UI_Stackpanel.Focus();
+        }
+
+        private void Window_Closed(object sender, CancelEventArgs e) {
+        }
+
+
+        private void initDownloadFolder() {
+            if(Settings.Default.DownloadFolder == "")
+                UI_DownloadFolder.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Downloads\\";
+            else UI_DownloadFolder.Text = Settings.Default.DownloadFolder;
+        }
+
+        private void UI_AddToDownload(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void UI_SelectExtensionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void UI_OpenCurrentDownloadFolder(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void UI_ResetFolder(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void UI_SelectNewDownloadFolder(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void UI_ClearDownloadList(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void UI_StartDownload(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void UI_DownloadFolder_LostFocus(object sender, RoutedEventArgs e) {
+
         }
     }
 }
